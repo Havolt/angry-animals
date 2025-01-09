47a8e5609796d278e282a15cde60db426b68bb36
Mark <markfitz815@gmail.com>
2025-01-09 22:36:22 +0000

    Make animal draggable

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
+enum ANIMAL_STATE { READY, DRAG, RELEASE }
+
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+
+@onready var label: Label = $Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +14,35 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	update(delta)
+	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+	
+	
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag") == true:
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+	return false
+
+func update_drag() -> void:
+	if detect_release() == true:
+		return
+	
+	var gmp = get_global_mouse_position()
+	position = gmp
+
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
 
 func die() -> void:
 	SignalManager.on_animal_died.emit()
@@ -16,3 +50,9 @@ func die() -> void:
 
 func _on_screen_exited() -> void:
 	die()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
+		
